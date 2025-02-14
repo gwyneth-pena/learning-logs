da9210a8106088a3acd558e35a6558b1159a29fc
unknown <gwenpena21@gmail.com>
2025-02-14 11:57:39 +0800

    Added Topic and Entry classes

---
+++ learning_logs/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    text = models.CharField(max_length=300)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        return self.text
