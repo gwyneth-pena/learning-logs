77da9862db2ad6b23a1a7b8c81396930f287430b
unknown <gwenpena21@gmail.com>
2025-02-14 15:22:52 +0800

    Added initial templates

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""URLS for Learning Logs"""
+
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics', views.topics, name='topics'),
+    path('topic/<int:topic_id>', views.topic, name='topic'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic
+
 # Create your views here.
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('-date_created')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_created')
+    context = {'entries': entries, 'topic': topic}
+    return render(request, 'learning_logs/topic.html', context)
