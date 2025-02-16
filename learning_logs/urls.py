fdc83cbbb2d459c7765e9b0346d49d5d26670425
unknown <gwenpena21@gmail.com>
2025-02-16 16:03:55 +0800

    Pages to add new entry and topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
+    path('topics/add_new', views.new_topic, name='new_topic'),
     path('topic/<int:topic_id>', views.topic, name='topic'),
+    path('topic/<int:topic_id>/add_entry', views.new_entry, name='new_entry'),
 ]
