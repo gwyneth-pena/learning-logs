d915ec23536e4e0609cbc7c9684530d1b11acd4b
unknown <gwenpena21@gmail.com>
2025-02-17 16:04:15 +0800

    Added deployment configs

--- learning_logs_proj/settings.py
+++ learning_logs_proj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'accounts',
     'learning_logs',
     'django_bootstrap5',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'learning_logs/static'),]
+STATIC_DIRS = [os.path.join(BASE_DIR,'learning_logs/static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
