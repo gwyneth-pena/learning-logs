d358f3b0f916bb329ce87a9754bde774b12b005e
unknown <gwenpena21@gmail.com>
2025-02-17 17:34:46 +0800

    Fixed settings

--- learning_logs_proj/settings.py
+++ learning_logs_proj/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'learning_logs_proj.wsgi.application'
 
 if os.getenv('VERCEL') == '1':  # Check if it's a Vercel environment
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT', 5432),
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
     }
 else:  # Local environment
     DATABASES = {
